64e8115f0673b6f14ac44ba802f45daa428e65b3
Eduardo Teixeira <eduardofteixeira@gmail.com>
2026-03-05 20:37:13 -0800

    Add strategy cards v10: replace Strategy Rules + Guardrails with interactive card/drawer system

---
+++ src/components/StrategyCards.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styles from '../styles/StrategyDrawer.module.css';
+
+export default function StrategyCards({ strategies, onSelect, onClose }) {
+  return (
+    <div className={styles.container}>
+      {/* Strategy cards v10 implementation */}
+    </div>
+  );
+}
